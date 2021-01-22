c222acc709d99d5a2890fe99fcb7fffe566b7d07
Sunny-jain <sunny1388.cse18@chitkara.edu.in>
2021-01-22 13:59:11 +0530

    control carousel movement using button

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 3000 } );
+    $("#carouselbutton").click(function(){
+        if($('#carouselbutton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselbutton').children('span').removeClass('fa-pause');
+            $('#carouselbutton').children('span').addClass('fa-play');
+        }
+        else{
+            $('#mycarousel').carousel('cycle');
+            $('#carouselbutton').children('span').removeClass('fa-play');
+            $('#carouselbutton').children('span').addClass('fa-pause');
+        }
+    });
+});
